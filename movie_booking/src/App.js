2646c59f996a76ec222275216673b3a98c1b1c2c
Nguyen_Nhat_Hao <nguyennhathao171@gmail.com>
2022-11-23 22:18:34 +0700

    add css

---
+++ movie_booking/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MovieBooking from './Booking/MovieBooking';
+
+function App() {
+  return (
+    <MovieBooking/>
+  );
+}
+
+export default App;
